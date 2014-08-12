a8d983e1643739f39d52201945d44087c7bd763a
jordan_weizmann <jvalansi.weizmann@gmail.com>
2014-08-12 21:35:32 +0300

    fixed clean

--- Machine_Translation/clean.py
+++ Machine_Translation/clean.py
@@ -4,29 +4,43 @@ Created on Jul 27, 2014
 @author: jordan_weizmann
 '''
 import argparse
+import re
+import os
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-e','--efile', type=argparse.FileType('r+'), 
+                       help='english file')
+    parser.add_argument('-f','--ffile', type=argparse.FileType('r+'), 
+                       help='foreign file')
+    parser.add_argument('-t','--threshold', type=int, default=60,
+                       help='threshold for sentences')
+    return(parser.parse_args())
 
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-i','--files', type=argparse.FileType('r+'), nargs=2, 
-                       help='files to clean')
-    parser.add_argument('-t','--threshold', type=int, default=60,
-                       help='threshold for sentences')
     
-    args = parser.parse_args()
-    content1 = args.files[0].readlines()
-    content2 = args.files[1].readlines()
-    args.files[0].seek(0)
-    args.files[0].truncate()
-    args.files[1].seek(0)
-    args.files[1].truncate()
-                    
-    l = min(len(content1),len(content2))
+    args = get_args()
+    e = args.efile.readlines()
+    f = args.ffile.readlines()
+    t = args.threshold
+    args.efile.close()
+    args.ffile.close()
+
+    et = open('etemp', 'w')
+    ft = open('ftemp', 'w')
+
+    pattern = r'(\S)(\W)'
+    repl = r'\g<1> \g<2>'
+    l = min(len(e),len(f))
     for i in range(l):
         #TODO:  space non words
-        if(len(content1[i].split()) > args.n or len(content2[i].split()) > args.n):
+        
+        e[i] = re.sub(pattern, repl, e[i])
+        if(len(e[i].split()) > t or len(f[i].split()) > t):
             continue
-        args.files[0].write(content1[i])
-        args.files[1].write(content2[i])
-
-
+        et.write(e[i])
+        ft.write(f[i])
+        
+    os.rename(et.name, args.efile.name)
+    os.rename(ft.name, args.ffile.name)
